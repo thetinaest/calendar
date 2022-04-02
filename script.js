095ac24866a8354422a5640a1203bf6d26c1a0dd
LittleToes <kristina.r.brennan@gmail.com>
2022-04-02 18:50:22 -0500

    save items to local storage and display each according to hour

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var now = moment();
-$('#currentDay').text(now.format('MMMM DD, YYYY'));
+var now = moment().format("MMMM DD, YYYY");
+var time = moment().hours();
+console.log(time);
+$('#currentDay').text(now);
+
+//save tasks to local storage
+$(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".task").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, task);
+})
+//display each task depending oh hour
+$("#9 .task").val(localStorage.getItem("9"));
+$("#10 .task").val(localStorage.getItem("10"));
+$("#11 .task").val(localStorage.getItem("11"));
+$("#12 .task").val(localStorage.getItem("12"));
+$("#13 .task").val(localStorage.getItem("13"));
+$("#14 .task").val(localStorage.getItem("14"));
+$("#15 .task").val(localStorage.getItem("15"));
+$("#16 .task").val(localStorage.getItem("16"));
+$("#17 .task").val(localStorage.getItem("17"));
+
+
+// function test() {
+//     var notes = window.localStorage.getItem('test');
+//     var b = JSON.parse(notes);
+//     console.log(b);
+//     $('.task').text = 0[task];
+// }
+
+// var notes = window.localStorage.getItem('test');
+// var b = JSON.parse(notes);
+// console.log(b);
+
