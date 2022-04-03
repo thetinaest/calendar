549d0941dc6bfc1732e79dfa00e12ce0dbf6f528
LittleToes <kristina.r.brennan@gmail.com>
2022-04-02 19:37:00 -0500

    color code hour blocks based on current time

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//globals
 var now = moment().format("MMMM DD, YYYY");
 var time = moment().hours();
-console.log(time);
+
+//variables used in for loop
+var timeBlock = $('.time-block');
+var counter = 9;
+
+//display current day in header
 $('#currentDay').text(now);
 
 //save tasks to local storage
@@ -9,7 +15,8 @@ $(".saveBtn").on("click", function () {
     var hour = $(this).parent().attr("id");
     localStorage.setItem(hour, task);
 })
-//display each task depending oh hour
+
+//display each task depending on hour
 $("#9 .task").val(localStorage.getItem("9"));
 $("#10 .task").val(localStorage.getItem("10"));
 $("#11 .task").val(localStorage.getItem("11"));
@@ -20,15 +27,21 @@ $("#15 .task").val(localStorage.getItem("15"));
 $("#16 .task").val(localStorage.getItem("16"));
 $("#17 .task").val(localStorage.getItem("17"));
 
-
-// function test() {
-//     var notes = window.localStorage.getItem('test');
-//     var b = JSON.parse(notes);
-//     console.log(b);
-//     $('.task').text = 0[task];
-// }
-
-// var notes = window.localStorage.getItem('test');
-// var b = JSON.parse(notes);
-// console.log(b);
-
+//color code hour blocks
+for (var i = 0; i < timeBlock.length; i++) {
+    //select the current timeBlock based on hour, starting at 9
+    var currentBlock = $('#' + counter);
+    //get id and turn it into an integer
+    var idHour = $('.time-block')[i].id
+    parseInt(idHour);
+    // check each id to see if it is past present or future
+    if (idHour < time) {
+        currentBlock.addClass("past");
+    } else if (idHour == time) {
+        currentBlock.addClass("present");
+    } else if (idHour > time) {
+        currentBlock.addClass("future");
+    }
+    //increase counter to select next time block
+    counter ++;
+}
